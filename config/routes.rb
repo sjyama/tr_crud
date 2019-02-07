27198426c0e7d3be00b369bd8aeb960dea2ea78a
sjyama_mbp <grinpeace101@gmail.com>
2019-02-08 05:49:57 +0900

    add edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
 	get "/users", to: "users#index"
 	get "/users/new", to: "users#new"
+	get "/users/:id/edit", to: "users#edit"
 	post "/users", to: "users#create"
+	patch "/users/:id", to: "users#update"
 	delete "/users/:id", to: "users#destroy"
 
 end
