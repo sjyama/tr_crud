f2c0cb8e427e8e09089f88bd61e7e0580237c1ac
sjyama_mbp <grinpeace101@gmail.com>
2019-02-13 16:49:55 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-	get "/users", to: "users#index"
-	get "/users/new", to: "users#new"
-	get "/users/:id/edit", to: "users#edit"
-	post "/users", to: "users#create"
-	patch "/users/:id", to: "users#update"
-	delete "/users/:id", to: "users#destroy"
+	get "/", to: "users#index"
+	# get "/users", to: "users#index"
+	# get "/users/new", to: "users#new"
+	# get "/users/:id/edit", to: "users#edit"
+	# post "/users", to: "users#create"
+	# patch "/users/:id", to: "users#update"
+	# delete "/users/:id", to: "users#destroy"
+  resources :users
 
 end
